098ea52939f0f82cb934b07b3619a26c9cecb766
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-03-19 22:48:06 -0400

    Update #1229MeetingScheduler.java

--- Intervals/#1229MeetingScheduler.java
+++ Intervals/#1229MeetingScheduler.java
@@ -1,2 +1,26 @@
 // https://leetcode.com/problems/meeting-scheduler/
 
+class Solution {
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        Arrays.sort(slots1, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(slots2, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        int i = 0, j = 0;
+        
+        while(i < slots1.length && j < slots2.length) {
+            int interval = Math.min(slots1[i][1], slots2[j][1]) - Math.max(slots1[i][0], slots2[j][0]);
+            if (interval >= duration) {
+                int min = Math.max(slots1[i][0], slots2[j][0]);
+                ArrayList<Integer> result = new ArrayList<Integer>(Arrays.asList(min, min + duration));
+                return result;
+            } 
+            
+            if (slots1[i][1] <slots2[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return new ArrayList<Integer>();
+    }
+}
