b3e0cd65abad25e8fc4c1a70cb174b61695475ea
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-03-27 23:29:02 -0400

    Create 1644. Lowest Common Ancestor of a Binary Tree II.java

---
+++ BST/1644. Lowest Common Ancestor of a Binary Tree II.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Use Stack to traverse the tree.
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        
+        // Use HashMap to track all parent nodes.
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        
+        stack.push(root);
+        parent.put(root, null);
+        
+        // Traverse tree to find both p and q.
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.push(node.left);
+            } 
+            
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.push(node.right);
+            } 
+        }
+        
+        if (parent.containsKey(p) && parent.containsKey(q)) {
+            // HashSet for all p's parent node.
+            Set<TreeNode> ancestors = new HashSet<>();
+
+            while (p != null) {
+                ancestors.add(p);
+                p = parent.get(p);
+            }
+
+            while (!ancestors.contains(q)) {
+                q = parent.get(q);
+            }
+
+            return q;
+        } else {
+            return null;
+        }
+        
+        
+    }
+}
