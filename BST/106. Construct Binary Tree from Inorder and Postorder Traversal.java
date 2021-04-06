48ed9cec47e703df5a3ac20f2da6e0e475bf1473
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-04-06 17:07:36 -0400

    Update 106. Construct Binary Tree from Inorder and Postorder Traversal.java

--- BST/106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ BST/106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -14,30 +14,34 @@
  * }
  */
 class Solution {
-    private int postorderIndex;
-    private Map<Integer, Integer> inorderMap;
-    
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        postorderIndex = postorder.length - 1;
-        inorderMap = new HashMap<>();
-        
-        for (int i = 0; i < inorder.length; i++) {
+  private int postorderIndex;
+
+  private HashMap<Integer, Integer> inorderMap;
+
+  
+
+  public TreeNode buildTree(int[] inorder, int[] postorder) {
+    inorderMap = new HashMap<>();
+
+    postorderIndex = postorder.length - 1;
+
+    for (int i = 0; i < inorder.length; i++) {
             inorderMap.put(inorder[i], i);
         }
-        
-        return arrayToTree(inorder, postorder, 0, inorder.length - 1);
-    }
+      
+    return arrayToTree(inorder, postorder, 0, inorder.length - 1);
+  }
     
-    private TreeNode arrayToTree(int[] inorder, int[] postorder, int left, int right) {
-        if(left > right) return null;
-        
-        int rootValue = postorder[postorderIndex];
-        postorderIndex--;
-        TreeNode root = new TreeNode(rootValue);
-        
-        root.left = arrayToTree(inorder, postorder, left, inorderMap.get(rootValue) - 1);
-        root.right = arrayToTree(inorder, postorder, inorderMap.get(rootValue) + 1, right);
-        
-        return root;
-    }
+ private TreeNode arrayToTree(int[] inorder, int[] postorder, int left, int right) {
+    if (left > right) return null;
+
+    int rootValue = postorder[postorderIndex--];
+    TreeNode root = new TreeNode(rootValue);
+
+
+    root.right = arrayToTree(inorder, postorder, inorderMap.get(rootValue) + 1, right);
+    root.left = arrayToTree(inorder, postorder, left, inorderMap.get(rootValue) - 1);
+      
+    return root;
+  }
 }
