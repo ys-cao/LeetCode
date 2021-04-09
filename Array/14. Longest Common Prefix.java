3bc5e7a9e6a1368f860ae14eb7c07840e8dbfd8d
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-04-08 22:51:19 -0400

    Create 14. Longest Common Prefix.java

---
+++ Array/14. Longest Common Prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        // Arrays.sort(strs, Comparator.comparingInt(String::length));
+        
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() == i || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        
+        return strs[0];
+    }
+}
